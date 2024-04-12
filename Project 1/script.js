2344c6fa9b98771a239799167e5b8e3fc2d2624e
ROHAN PRAMANIK <121264513+rohan-58@users.noreply.github.com>
2024-04-12 16:39:56 +0000

    project 1

---
+++ Project 1/script.js
@@ -0,0 +1,25 @@
+// There will be four box of different color, if we click any of he box the back ground will be same as box color
+
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(buttons);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      //if we press white the back ground will be black
+      body.style.backgroundColor = 'black';
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
