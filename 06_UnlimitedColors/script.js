d1f0f082d2355c69a9d02c6ab6f5cc08c132b988
ROHAN PRAMANIK <121264513+rohan-58@users.noreply.github.com>
2024-05-16 03:42:55 +0000

    new upadte

---
+++ 06_UnlimitedColors/script.js
@@ -0,0 +1,34 @@
+//generate a random color
+const randomColor  = function(){
+    const hex = "0123456789ABCDEF";
+    let color = '#';
+    for(let i=0;i<6;i++){
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+};
+
+// start changing color
+let intervalId  //for global scope
+const startChangingColor = function(){
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor, 1000);
+    }
+    
+
+    function changeBgColor(){        
+        document.body.style.backgroundColor = randomColor();
+    }
+    
+}
+
+//  stop changing color
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+
+
+document.querySelector("#start").addEventListener('click', startChangingColor)
+document.querySelector("#stop").addEventListener('click', stopChangingColor)
